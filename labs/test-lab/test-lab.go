44a4c62a1b4c89cc8694a61a3d04a55556bfcadb
Andres Salas Villarreal <a01632107@itesm.mx>
2021-02-18 01:39:14 +0000

    Solve test-lab
    Signed-off-by: Andres Salas Villarreal <a01632107@itesm.mx>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"errors"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  argsSize := len(os.Args)
+  if argsSize > 1 {
+    fmt.Print("Hello ")
+    i := 1
+    for i <= argsSize - 1 {
+      fmt.Print(os.Args[i])
+      fmt.Print(" ")
+      i = i + 1
+    }
+    fmt.Print(", Welcome to the Jungle \n")
+  } else {
+    err := errors.New("Error, Missing Name")
+		if err != nil {
+		  fmt.Println(err)
+	  }
+  }
 }
